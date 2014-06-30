77bf6f3ead3479d8a3832e0a10b72651d5e00a2a
Alejandro E. Losada <alosada@gmail.com>
2014-06-29 20:37:17 -0700

    Added test for question 2

--- euler.rb
+++ euler.rb
@@ -8,10 +8,17 @@ class Euler
     return res
   end
 
+  def sum_even_fib(limit)
+
+  end
+
 end
 
 
 ### SIMPLE TESTS ###
 
-print "Multiples of 3 & 5 are equal to 23: "
+print "Q1: Multiples of 3 & 5 are equal to 23: "
 p Euler.multiples(10)==23
+
+print "Q2: Sum of first 10 even fibonnacci numbers is 44: "
+p Euler.sum_even_fib(89)==44
